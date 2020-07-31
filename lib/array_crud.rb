d6deea2ca85105e1b0e9233f5dd2721791c7f6d3
Kyle Stough <kstough00@gmail.com>
2020-07-31 21:09:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  my_actual_array_2.push("rabbit")
+  create_an_array << "rabbit"
 end
 
 def add_element_to_start_of_array(array, element)
