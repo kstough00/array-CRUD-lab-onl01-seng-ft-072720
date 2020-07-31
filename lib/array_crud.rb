28a6da1dd0ba73052bec06f96f0ccba3b4cb82e9
Kyle Stough <kstough00@gmail.com>
2020-07-31 21:00:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  my_actual_array = 
+  my_actual_array = ["dog", "cat", "fish", "turtle"]
 end
 
 def add_element_to_end_of_array(array, element)
